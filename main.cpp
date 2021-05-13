7419e23fd6b01697d6836e3fd99221526d25585e
pcarzon <pcarzon@ltu.edu>
2021-05-13 11:05:29 -0500

    test

--- main.cpp
+++ main.cpp
@@ -34,10 +34,10 @@ default_random_engine get_random_number;
 int main (int argc, char *argv[])
 {
 	double e_out, w_tilde;
-	GreensFunctions::GetValues(1, 1, 0.5*M_PI, e_out, w_tilde);
+	GreensFunctions::GetValues(0.2, 1, 0.5*M_PI, e_out, w_tilde);
 	cout << "Testing BackgroundAttractor e_out = " << e_out << " w_tilde = " << w_tilde << endl;
 	return 0;
-	
+
 	IO inOut(argv[1]);
 
 
