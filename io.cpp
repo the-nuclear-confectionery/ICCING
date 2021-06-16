292bfe62aa3482807c586aa1c758f9ab32471e22
pcarzon <pcarzon@ltu.edu>
2021-06-16 09:24:10 -0500

    test

--- io.cpp
+++ io.cpp
@@ -805,9 +805,9 @@ Event IO::ReadEvent(Event event_in)
         int y = event_in.valued_points[i][1];
         double evolved_energy = 0, wtilde = 0;
         cout << "x " << x << " y " << y << endl;
-        cout << "energy " << 1.0e-2 << " tau_hydro " << tau_hydro << " eta_over_s " << eta_over_s << endl;
+        cout << "energy " << 10.0 << " tau_hydro " << tau_hydro << " eta_over_s " << eta_over_s << endl;
   //      event_in.evolution.GetValues(tau_0*event_in.initial_energy[x][y], tau_hydro, eta_over_s, evolved_energy, wtilde);
-        event_in.evolution.GetValues(1.0e-2, tau_hydro, eta_over_s, evolved_energy, wtilde);
+        event_in.evolution.GetValues(10.0, tau_hydro, eta_over_s, evolved_energy, wtilde);
         event_in.density[0][x][y] = evolved_energy;
         event_in.w_tilde[x][y] = wtilde;
         cout << "Evolved energy " << event_in.density[0][x][y] << " Original energy " << tau_0*event_in.initial_energy[x][y] << " w_tilde " << event_in.w_tilde[x][y] << endl;
