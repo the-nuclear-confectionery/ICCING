0fdafee08931643a512ef9d6a7fdd41e29d1074f
pcarzon <pcarzon@ltu.edu>
2021-08-26 10:11:16 -0500

    test

--- event.cpp
+++ event.cpp
@@ -322,7 +322,10 @@ bool Event::UpdateDensity(Quarks quark_density)
           cout << "test 4 " << greens_dist.size() << endl;
           cout << quark_bounds[0] << " " << quark_bounds[1] << " " << quark_bounds[2] << " " << quark_bounds[3] << endl;
           cout << i << " " << j << endl;
-          cout << greens_dist[i][j] << " " << final_energy_backup[i][j] << " " << initial_energy_backup[i][j] << " " << evolution.Gss(w_tilde[i][j], quark_distance/(tau_hydro - tau_0)) << endl;
+          cout << greens_dist[i][j] << endl;
+          cout << final_energy_backup[i][j] << endl;
+          cout << initial_energy_backup[i][j] << endl;
+          cout << evolution.Gss(w_tilde[i][j], quark_distance/(tau_hydro - tau_0)) << endl;
           //  Energy = alpha*(E_glueon/E_tot)*E_tot*quark_dist
           density[0][temp_x][temp_y] += quark_density.GetAlpha()*(quark_density.GetEnergyFraction()*out_sample.e_tot)*greens_dist[i][j]
                                         *(final_energy_backup[i][j]/initial_energy_backup[i][j])
