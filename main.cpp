7818e87b9f0a5682729b260ead866dc6903f6a82
pcarzon <pcarzon@ltu.edu>
2021-08-27 10:07:59 -0500

    Testing Strange Quark Evolution

--- main.cpp
+++ main.cpp
@@ -99,7 +99,7 @@ int main (int argc, char *argv[])
 				//  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 				//	Update Density grids with sampled quarks, if quarks are out of bounds of grid, sample event again
-				if (!testEvent.UpdateDensity(testQuarks)) { continue; }
+				if (!testEvent.UpdateDensity(testQuarks)) { break; }
 
 			}
 
