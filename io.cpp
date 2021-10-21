aa5e1286319f44e0b8e42d547ac0feb5cb26c4b6
pcarzon <pcarzon@ltu.edu>
2021-10-21 09:56:50 -0500

    Minor fix

--- io.cpp
+++ io.cpp
@@ -906,7 +906,7 @@ Event IO::ReadEvent(Event event_in)
         y = (int)round((ready + grid_max)/grid_step);
 
         //  Set point in event's initial energy density grid
-        event_in.t_b[x][y] = value;
+        event_in.t_b[x][y] = kappa_*sqrt(value);
         input.ignore(10000, '\n');  //  Ignore rest of line
         if (input.peek() == '\n') {break;}  //  Saftey check for empty line at end of file
       }
