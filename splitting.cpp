f50b9702a743171441db9c40ae10ac2f68c4a798
pcarzon <pcarzon@ltu.edu>
2022-03-28 12:38:20 -0500

    test

--- splitting.cpp
+++ splitting.cpp
@@ -234,7 +234,7 @@ vector<double> Splitter::RollLocation(double mass, double Qs)
   }
 
   if (test_ == "SingleQuark")
-  { r_final = 2; }
+  { r_final = 4; }
 
   //  Get random phi to calculate the x and y displacement for the quark pair
   phi = get_phi(get_random_number);
