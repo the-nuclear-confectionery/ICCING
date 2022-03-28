b008ca99f302e3bdcf49a6bf851e55afab2708a3
pcarzon <pcarzon@ltu.edu>
2022-03-28 12:12:02 -0500

    minor fix

--- event.cpp
+++ event.cpp
@@ -474,7 +474,7 @@ cout << "this is greens_evolution " << greens_evolution << endl;
           temp_y = y_center - greens_rad + j;
           double gluon_distance = sqrt(pow((x_center - temp_x)*grid_step, 2) + pow((y_center - temp_y)*grid_step, 2));
 
-          density[0][temp_x][temp_y] += (quark_density.GetEnergyFraction()*out_sample.e_tot)*greens_dist[i][j]
+          density[0][temp_x][temp_y] -= (quark_density.GetEnergyFraction()*out_sample.e_tot)*greens_dist[i][j]
                                         *(final_energy_backup[x_center][y_center]/initial_energy_backup[x_center][y_center])
                                         *evolution.Gss(w_tilde[x_center][y_center], gluon_distance/(tau_hydro - tau_0));
 
