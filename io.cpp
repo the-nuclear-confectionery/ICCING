8f02f17aca80715901c59714ed8f3679af1035a6
pcarzon <pcarzon@ltu.edu>
2022-03-29 10:47:01 -0500

    test

--- io.cpp
+++ io.cpp
@@ -981,7 +981,7 @@ Event IO::ReadEvent(Event event_in)
         input >> readx >> ready >> value;
 
         if (sub_test == "SmoothProfile")
-        { value = 0.2; }
+        { value = 0.06; }
 
         //  Take physical point and convert x and y values into grid indicies
         x = (int)round((readx + grid_max)/grid_step);
