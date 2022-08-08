d9d825518022f4f2ad64ff767c0b99fd281016f3
pcarzon <pcarzon@ltu.edu>
2022-08-08 13:50:39 -0500

    Changed OutputEccentricities to  print all radii and norms

--- io.cpp
+++ io.cpp
@@ -805,11 +805,11 @@ void IO::OutputEccentricities(double total_entropy, vector<vector<double>> eccen
     //  Print magnitude and angle of eccentricities
     for (int i = 0; i < eccentricities.size(); i++)
     {
-      output << eccentricities[i][0] << " " << eccentricities[i][1] << " ";
+      output << eccentricities[i][0] << " " << eccentricities[i][1] << " " << eccentricities[i][2];
     }
 
     //  Output radius
-    output << eccentricities[0][2] << endl;
+    output << endl;
 
     output.close();
   }
